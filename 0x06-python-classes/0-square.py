6dd2283db27d3907d3a8d759f39489e4801381a9
meryemouirem <meryemouirem94@gmail.com>
2023-08-29 16:10:39 -0700

    r

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-"""Square module."""
+"""Define a class Square."""
+
 
 class Square:
-    """Defines a square."""
+    """Represent a square."""
     pass
--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
-"""Square module."""
+"""Define a class Square."""
+
 
 class Square:
-    """Defines a square."""
+    """Represent a square."""
 
-    def __int__(self, size):
-        """constructor..
+    def __init__(self, size):
+        """Initialize a new Square.
 
         Args:
-            size: length of a side of the square.
+            size (int): The size of the new square.
         """
         self.__size = size
